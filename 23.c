ab709d58a0ca81b063cc9cafcc494159f7063c87
kimgang <yangsangchoo1112@gmail.com>
2025-08-28 23:10:59 +0900

    23 문제 완료

---
+++ 23.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    int arr[n][m];
+    int num = 1;
+
+   
+    for (int diag = 0; diag < n + m - 1; diag++) {
+        int row_start = (diag < n) ? diag : n - 1;
+        int col_start = diag - row_start;
+
+        while (row_start >= 0 && col_start < m) {
+            arr[row_start][col_start] = num++;
+            row_start--;
+            col_start++;
+        }
+    }
+
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
