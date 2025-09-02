5f03a5dd6f8d014560d0bf69cb08648e9df88a2b
kimgang <yangsangchoo1112@gmail.com>
2025-09-02 22:59:03 +0900

    27 문제 완료

--- 27.c
+++ 27.c
@@ -64,3 +64,69 @@ int main() {
 }
 
 
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    if (n == 0) {
+        printf("0\n");
+        return 0;
+    }
+
+    int freq[31] = {0};
+    int i;
+    for (i = 0; i < n; i++) {
+        int x;
+        scanf("%d", &x);
+        freq[x]++;
+    }
+
+    int cut = (int)round(n * 0.15);
+    int total = 0;
+    int count = 0;
+
+
+    i = 1;
+    int rem = cut;
+    while (rem > 0 && i <= 30) {
+        if (freq[i] <= rem) {
+            rem -= freq[i];
+            freq[i] = 0;
+        } else {
+            freq[i] -= rem;
+            rem = 0;
+        }
+        i++;
+    }
+
+
+    i = 30;
+    rem = cut;
+    while (rem > 0 && i >= 1) {
+        if (freq[i] <= rem) {
+            rem -= freq[i];
+            freq[i] = 0;
+        } else {
+            freq[i] -= rem;
+            rem = 0;
+        }
+        i--;
+    }
+
+
+    for (i = 1; i <= 30; i++) {
+        total += freq[i] * i;
+        count += freq[i];
+    }
+
+    double avg = (double)total / count;
+    int result = (int)round(avg);
+    printf("%d\n", result);
+
+    return 0;
+}
+
+
