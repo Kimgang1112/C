7aa92238c4a116daa07bba7fbb01f57b8a40659d
kimgang <yangsangchoo1112@gmail.com>
2025-09-10 23:09:46 +0900

    44 문제 완료

---
+++ 44.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    int n;
+    scanf("%d",&n);
+
+    int *arr;
+    int sum=0;
+
+    arr=(int *)malloc(n*sizeof(int));
+
+    for (int i=0; i<n; i++){
+        scanf("%d",&arr[i]);
+    }
+
+    for (int i=0; i<n; i++){
+        sum+=arr[i];
+    }
+
+    printf("%d",sum);
+
+
+
+}
