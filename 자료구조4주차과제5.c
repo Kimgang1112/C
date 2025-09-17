04c1c3cf719c064e4e0479af53c421fda0c183f8
kimgang <yangsangchoo1112@gmail.com>
2025-09-17 23:31:37 +0900

    자료구조 4주차 과제 5

---
+++ 자료구조4주차과제5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    char name[30];
+    int score;
+} Student;
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    Student *students = (Student *)malloc(N * sizeof(Student));
+    if (students == NULL) {
+        printf("Memory allocation failed\n");
+        return 1;
+    }
+
+    int sum = 0;
+    for (int i = 0; i < N; i++) {
+        scanf("%s %d", students[i].name, &students[i].score);
+        sum += students[i].score;
+    }
+
+    double avg = (double)sum / N;
+    printf("%.2f\n", avg);
+
+    free(students);
+    return 0;
+}
