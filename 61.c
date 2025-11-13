7413d1ccc73984d8087d56f54f938f66b3f9770f
kimgang <yangsangchoo1112@gmail.com>
2025-11-13 19:08:23 +0900

    반복문 중위순회

--- 61.c
+++ 61.c
@@ -1,46 +1,47 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-typedef int element;
-
-typedef struct Node {
-    element data;
+typedef struct Node{
+    int data;
     struct Node* left;
     struct Node* right;
 } Node;
 
 
-void inorder(Node* root) {
-    Node* stack[100]; 
-    int top = -1;
-    Node* current = root;
+void inorder(Node* root){
 
-    while (current != NULL || top != -1) {
-        while (current != NULL) {
-            stack[++top] = current;
-            current = current->left;
+    Node* stack[100];
+    int top=-1;
+
+    Node* current=root;
+
+    while (current!=NULL || top!=-1){
+        while (current!=NULL){
+            stack[++top]=current;
+            current=current->left;
         }
-        current = stack[top--];
-        printf("%d ", current->data);
-        current = current->right;
+        printf("%d",stack[top]->data);
+        current=stack[top--]->right;
+        
     }
+
 }
 
 
-int main(void) {
-    Node N7 = {70, NULL, NULL};
-    Node N6 = {60, NULL, NULL};
-    Node N5 = {50, NULL, NULL};
-    Node N4 = {40, NULL, NULL};
-    Node N3 = {30, &N6, &N7};
-    Node N2 = {20, &N4, &N5};
-    Node N1 = {10, &N2, &N3};
 
-    Node* root = &N1;
 
-    
-    inorder(root);
-    
+int main(void){
+
+    Node N7 = {7,NULL,NULL};
+    Node N6 = {6,NULL,NULL};
+    Node N5 = {5,NULL,NULL};
+    Node N4 = {4,NULL,NULL};
+    Node N3 = {3,&N6,&N7};
+    Node N2 = {2,&N4,&N5};
+    Node N1 = {1,&N2,&N3};
+
+    inorder(&N1);
+
+
 
     return 0;
 }
