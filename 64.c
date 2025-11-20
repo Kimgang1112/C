f015edf12aa002cf63aa8ebf04f877aa99b6f17e
kimgang <yangsangchoo1112@gmail.com>
2025-11-20 09:26:26 +0900

    이진탐색트리 탐색 반복문 구현

---
+++ 64.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+typedef struct Node{
+    int data;
+    struct Node* left;
+    struct Node* right;
+}Node;
+
+
+Node* search_data(Node * root, int d){
+    while (root!=NULL){
+        if (root->data==d){
+            return root;
+        } else if (root->data>d){
+            root=root->left;
+        } else{
+            root=root->right;
+        }
+    }
+    return NULL;
+
+
+}
+
+int main(void){
+    Node N7={70,NULL,NULL};
+    Node N6={50,NULL,NULL};
+    Node N5={30,NULL,NULL};
+    Node N4={10,NULL,NULL};
+    Node N3={60,&N6,&N7};
+    Node N2={20,&N4,&N5};
+    Node N1={40,&N2,&N3};
+
+    printf("%d",search_data(&N1,30)->data);
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
